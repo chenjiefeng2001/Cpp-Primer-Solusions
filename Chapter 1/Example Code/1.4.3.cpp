7aec35e0e15031a1480a6f8415b4e76383881e3a
Chenjiefeng <1497735430@qq.com>
2021-11-10 14:27:11 +0800

    update

---
+++ Chapter 1/Example Code/1.4.3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    int sum = 0, value = 0;
+    while (std::sin >> value)
+    {
+        sum += value;
+        std::cout << "Sum is :" << sum << std::endl;
+    }
+    return 0;
+}
