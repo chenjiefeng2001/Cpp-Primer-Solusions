44e288422061dc9a9ac4a9d5962027fb39899dc5
Chenjiefeng <1497735430@qq.com>
2021-11-10 11:03:43 +0800

    update

---
+++ Chapter 1/1.6.cpp
@@ -0,0 +1,12 @@
+//error code
+#include <iostream>
+int main()
+{
+    std::cout<<"The sum of "<<v1;
+            <<" and"<<v2;
+            <<" is "<<v1+v2<<std::endl;
+}
+/*
+* 这个程序是非法的，因为分隔符的使用不正确，应该只在最后的endl末尾加分号
+*
+*/
