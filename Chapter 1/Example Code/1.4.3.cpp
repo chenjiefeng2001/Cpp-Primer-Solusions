e3918b4602b265c8efe13df8cad057b86d37d6b8
Chenjiefeng <1497735430@qq.com>
2021-11-11 16:59:52 +0800

    update

--- Chapter 1/Example Code/1.4.3.cpp
+++ Chapter 1/Example Code/1.4.3.cpp
@@ -2,7 +2,7 @@
 int main()
 {
     int sum = 0, value = 0;
-    while (std::sin >> value)
+    while (std::cin >> value)
     {
         sum += value;
         std::cout << "Sum is :" << sum << std::endl;
