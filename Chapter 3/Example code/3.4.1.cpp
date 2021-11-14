ec5c68871f04976f866342c21267005e5032d0e5
Chenjiefeng <1497735430@qq.com>
2021-11-14 12:16:23 +0800

    update chapter 3

---
+++ Chapter 3/Example code/3.4.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::vector;
+using namespace std;
+int main()
+{
+    string s("some arting");
+    if (s.begin() != s.end())//判断首尾容器是否是相同，来确定是否非空
+        auto it =s.end();
+        *it = toupper(*it)//指向末尾元素
+        return 0;
+}
+/**
+ * 当然，迭代器也可以用于其他的操作，例如元素移位之类的操作。
+ */
+
