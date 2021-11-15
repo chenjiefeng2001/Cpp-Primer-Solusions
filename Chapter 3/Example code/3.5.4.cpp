e89a9801db8444f829add4a6e0b71ff9b7aa1f3a
Chenjiefeng <1497735430@qq.com>
2021-11-15 12:23:47 +0800

    update

--- Chapter 3/Example code/3.5.4.cpp
+++ Chapter 3/Example code/3.5.4.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
+
 int main()
 {
-    char ca[] = {'C', '+', '+'};
-    cout << "hello world" << endl;
+    char ca[]={'C','+','+'};
+    cout<<strlen(ca)<<endl;
+    getchar();
     return 0;
 }
