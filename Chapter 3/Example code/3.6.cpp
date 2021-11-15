821806641f1a3cc1a60a49156d171aa31f4bce1a
Chenjiefeng <1497735430@qq.com>
2021-11-15 13:13:51 +0800

    update

---
+++ Chapter 3/Example code/3.6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[10][20][30] = {0};
+    int ia[3][4];
+    ia[2][3] = arr[0][0][0];
+    int (&row)[4]=ia[i]
+    size_t cnt = 0;
+    for (auto &col)
+        return 0;
+}
