2af1ad5d8d1eb36493908cfea457dabad4afa635
Chenjiefeng <1497735430@qq.com>
2021-11-18 15:36:46 +0800

    update

--- Chapter 5/Example Code/5.6-2.cpp
+++ Chapter 5/Example Code/5.6-2.cpp
@@ -9,7 +9,16 @@ int main()
         {
             //执行添加两个Sales_item对象的代码
             //如果添加失败，代码会抛出一个runtime_error异常错误
-        }catch(runtime_error& err)
-
-        
+        }
+        catch (runtime_error &err)
+        {
+            //提醒用户两个ISBN必须一致，询问是否重新输入
+            cout << err.what()
+                 << "\n Try Again? Enter y or n" << endl;
+            char c;
+            cin >> c;
+            if (!cin || c == 'n')
+                break;
+            
+        }
 }
